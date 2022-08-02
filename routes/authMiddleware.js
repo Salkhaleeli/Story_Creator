d8f5473cf210fc75d7c01abf4d7b46c6033262c8
vagrant <eng.saad.najm@gmail.com>
2022-08-02 00:27:09 +0000

    adding middleware ad middleware redirect

---
+++ routes/authMiddleware.js
@@ -0,0 +1,32 @@
+const auth = (db) =>{
+  return (req,res,next) => {
+
+    const userName = req.session["username"];
+
+    console.log("--> user:", userName);
+
+    // responds with redirect if no sessions
+    if (!userName) {
+      console.log("--> auth: not logged in");
+      res.json({redirect: "/register"});
+      return;
+    }
+
+    const queryString = `SELECT id FROM users 
+    WHERE users.username = $1`;
+
+    db.query(queryString, [ userName ])
+      .then(data =>{
+        if (data.rows[0]) {
+          req.session.userId = data.rows[0].id;
+          return next();
+        } else {
+          console.log("--> auth: not logged in");
+          return res.redirect('/register');
+        }
+      });
+  };
+};
+
+
+module.exports = auth;
---
+++ routes/authMiddlewareRedirect.js
@@ -0,0 +1,31 @@
+const auth = (db) =>{
+  return (req,res,next) => {
+
+    const userName = req.session["username"];
+
+    console.log("--> user:", userName);
+
+    // responds with redirect if no sessions
+    if (!userName) {
+      console.log("--> auth-redirect: not logged in");
+      return res.redirect('/register');
+    }
+
+    const queryString = `SELECT id FROM users 
+    WHERE users.username = $1`;
+
+    db.query(queryString, [ userName ])
+      .then(data =>{
+        if (data.rows[0]) {
+          req.session.userId = data.rows[0].id;
+          return next();
+        } else {
+          console.log("--> auth-redirect: not logged in");
+          return res.redirect('/register');
+        }
+      });
+  };
+};
+
+
+module.exports = auth;
